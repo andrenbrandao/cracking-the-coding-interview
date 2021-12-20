3cecb7980a1afbda4e1f45cdaea73d56d0c00959
André Brandão <andrenbrandao@gmail.com>
2021-12-20 09:29:09 -0300

    arrays-and-strings: add skeleton for 1.2

---
+++ 1-arrays-and-strings/2-check-permutation/solution.py
@@ -0,0 +1,29 @@
+"""
+Problem:
+
+1.2 Given two strings, write a method to decide if one is a permutation of the other.
+"""
+
+"""
+Questions:
+
+
+"""
+
+
+def is_permutation(str1, str2):
+    pass
+
+
+def test(input_data, expected_answer):
+    answer = is_permutation(input_data)
+
+    if answer != expected_answer:
+        raise Exception(
+            f"Answer {answer} is wrong. Expected answer is {expected_answer}"
+        )
+
+
+if __name__ == "__main__":
+
+    print("All tests passed!")
